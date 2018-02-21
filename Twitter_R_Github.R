e6546dc4c62378a7b437884a25f92b6ca6f3a8cd
K. Hazel Kwon <k.hazel.kwon@gmail.com>
2018-02-21 13:23:27 -0700

    Update Twitter_R_Github.R

--- Twitter_R_Github.R
+++ Twitter_R_Github.R
@@ -19,7 +19,8 @@ setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
 #get tweets by searchTwitter function
 #Use Advanced search function from Twitter if you want to specify more search parameters
-tweet <- searchTwitter("SearchKeyword SpecificParameter", n=10000, lang = "en"  )
+##The user -tweet merge can be easily done upto around 15000 tweets.
+tweet <- searchTwitter("SearchKeyword SpecificParameter", n=15000, lang = "en"  )
 
 #Convert the list to dataframe
 tweetdf <- twListToDF(tweet)
@@ -32,9 +33,11 @@ length(user)
 #convert the list to dataframe
 userdf <- twListToDF(user) 
 
-#Merge by "column-bind" between tweet_df and user_info_df 
-##Make sure that users are in the same order between the two files.
-tweet_final <- cbind(tweetdf, userdf)
+#Merge the two by joining
+tweet_final <- merge(tweetdf, userdf, by = "screenName")
 
-#Save it into csv format.
-write.csv(tweet_final, file = '~/Desktop/tweet_final.csv', row.names = F)
+#Save it into csv format
+##for MAC
+write.csv(tweet_final, "path/tweet_final.csv", row.names = F)
+##for PC
+write.csv(tweet_final, "path\tweet_final.csv", row.names = F)
